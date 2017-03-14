9b7f66670fcce7711b259e6a95f80ef99e8011d4
James Sevedge <j.sevedge@f5.com>
2017-03-14 09:42:59 -0700

    initial waf checkin

--- scripts/autoscale.sh
+++ scripts/autoscale.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ARGS=`getopt -o r:v:u:p:s:m:a: --long resourceGroup:,vmssName:,userName:,password:,azureSecretFile:,managementPort:,autoPortCheckFlag: -n $0 -- "$@"`
+ARGS=`getopt -o r:v:u:p:s:m: --long resourceGroup:,vmssName:,userName:,password:,azureSecretFile:,managementPort: -n $0 -- "$@"`
 eval set -- "$ARGS"
 # Parse the command line arguments
 while true; do
