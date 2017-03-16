15d96651edf2f02002ba9ce48af7855797a53452
James Sevedge <j.sevedge@f5.com>
2017-03-16 15:25:48 -0700

    Fix typos...

--- scripts/autoscalewaf.sh
+++ scripts/autoscalewaf.sh
@@ -51,14 +51,14 @@ if [ -f /config/cloud/master ]; then
         tmsh create sys icall script ClusterUpdate definition { exec f5-rest-node /config/cloud/node_modules/f5-cloud-libs/scripts/azure/runScripts.js --base-dir /config/cloud/node_modules/f5-cloud-libs --log-level debug --autoscale "--cloud azure --log-level debug --output /var/log/azure-autoscale.log --host $selfip --port $mgmt_port --user $user --password-url file://$passwd_file --provider-options scaleSet:$vmss_name,azCredentialsUrl:file://$azure_secret_file,resourceGroup:$resource_group --cluster-action update --device-group Sync" }
         tmsh create sys icall handler periodic /Common/$icall_handler_name { first-occurrence now interval 300 script /Common/ClusterUpdate }
         tmsh save /sys config
-    else;
+    else
         echo "Appears the $icall_handler_name icall already exists!"
     fi
 
     # Deploy the WAF Application if master and ucs loaded equals false
 	if $ucs_loaded; then
         echo "NOTE: We are not deploying any WAF applications as a UCS was loaded, and it takes precedence."
-    else;
+    else
         /usr/bin/f5-rest-node /config/cloud/node_modules/f5-cloud-libs/scripts/azure/runScripts.js --base-dir /config/cloud/node_modules/f5-cloud-libs --script " --output /var/log/deployScript.log --log-level debug --file /var/lib/waagent/custom-script/download/0/deploy_waf.sh --cl-args '$waf_script_args' --signal DEPLOY_SCRIPT_DONE "
     fi
 
