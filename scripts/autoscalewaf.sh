18679c4d3e6bfea62ead1ce64b46e01ac507e8bd
James Sevedge <j.sevedge@f5.com>
2017-03-20 12:37:24 -0700

    Add while loop for instance check

--- scripts/autoscalewaf.sh
+++ scripts/autoscalewaf.sh
@@ -35,6 +35,16 @@ echo "WAF_SCRIPT: $waf_script_args"
 dfl_mgmt_port=`tmsh list sys httpd ssl-port | grep ssl-port | sed 's/ssl-port //;s/ //g'`
 selfip=$(tmsh list net self self_1nic address | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
 instance=`curl http://169.254.169.254/metadata/v1/InstanceInfo --silent --retry 5 | jq .ID | sed 's/_//;s/\"//g'`
+# Add check/loop in case metadata service does not respond right away
+count=0
+while [ $count -lt 5 ]; do
+    if [[ -z $instance ]]; then
+        sleep 5
+        instance=`curl http://169.254.169.254/metadata/v1/InstanceInfo --silent --retry 5 | jq .ID | sed 's/_//;s/\"//g'`
+    fi
+    count=$(( $count + 1 ))
+done
+echo "INSTANCE NAME CHOSEN: $instance"
 
 f5-rest-node /config/cloud/node_modules/f5-cloud-libs/scripts/azure/runScripts.js --base-dir /config/cloud/node_modules/f5-cloud-libs --log-level debug --onboard "--output /var/log/onboard.log --log-level debug --host $selfip --port $dfl_mgmt_port --ssl-port $mgmt_port -u $user --password-url file://$passwd_file --hostname $instance.azuresecurity.com --db provision.1nicautoconfig:disable --db tmm.maxremoteloglength:2048 --module ltm:nominal --module asm:nominal --module afm:none --signal ONBOARD_DONE" --autoscale "--wait-for ONBOARD_DONE --output /var/log/autoscale.log --log-level debug --host $selfip --port $mgmt_port -u $user --password-url file://$passwd_file --cloud azure --provider-options scaleSet:$vmss_name,azCredentialsUrl:file://$azure_secret_file,resourceGroup:$resource_group --cluster-action join --device-group Sync --block-sync"
 
