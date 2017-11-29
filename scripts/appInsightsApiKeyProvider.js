874f47f11416ff63d1aa43648af14e33394b3bd4
James Sevedge <j.sevedge@f5.com>
2017-11-29 10:02:28 -0800

    Refactor apiKeyProvider

--- scripts/appInsightsApiKeyProvider.js
+++ scripts/appInsightsApiKeyProvider.js
@@ -18,7 +18,7 @@ if (fs.existsSync('/config/cloud/.azCredentials')) {
     var credentialsFile = JSON.parse(fs.readFileSync('/config/cloud/.azCredentials', 'utf8'));
 }
 else {
-    logger.info('Credentials file not found');
+    logger.error('Credentials file not found');
     return;
 }
 
@@ -47,72 +47,144 @@ var logger = Logger.getLogger({logLevel: options.logLevel, fileName: '/var/log/a
 var credentials = new msRestAzure.ApplicationTokenCredentials(clientId, tenantId, secret);
 var client = new appInsights(credentials, subscriptionId);
 
-logger.info('App Insights ID: ' + appInsightsId);
+logger.info('App Insights ID:', appInsightsId);
 
+/**
+ * Check if operation is create, delete or list and act accordingly
+ */
 if ( options.keyOperation == "create" ) {
-    /**
-     * Create App Insights API Key
-     */
-    var apiKeyProperties = {};
+    Promise.all([
+        createAppInsightApiKey(resourceGroupName, appInsightsResourceName)
+    ])
+    .then((results) => {
+        logger.debug('List Response:', results[0]);
+        logger.info('API Key Name:', results[0].name);
+        logger.info('API Key ID:', results[0].id.split('/apikeys/')[1]);
+        logger.info('API Key:', results[0].apiKey);
+    })
+    .catch(err => {
+        logger.error('Error:', err);
+    });
+} else if ( options.keyOperation == "delete" ) {
+    Promise.all([
+        deleteAppInsightApiKey(resourceGroupName, appInsightsResourceName, options.keyId)
+    ])
+    .then((results) => {
+        logger.info('Delete Response:', results[0]);
+    })
+    .catch(err => {
+        logger.error('Error:', err);
+    });
+}
+if ( options.keyOperation == "list" || options.logLevel == "debug" || options.logLevel == "silly" ) {
+    Promise.all([
+        listAppInsightInstances(),
+        listAppInsightApiKeys(resourceGroupName, appInsightsResourceName)
+    ])
+    .then((results) => {
+        logger.info('List of App Insight components:', results[0]);
+        logger.info('List of API keys:', results[1]);
+    })
+    .catch(err => {
+        logger.error('Error:', err);
+    });
+}
+
+/**
+* Create App Insights API Key
+*
+* @returns {Promise}
+*/
+function createAppInsightApiKey(rgName, resourceName) {
     var date = Date.now();
+    var apiKeyProperties = {};
     apiKeyProperties.name = 'apikeysdk' + date;
 
     var basePropertiesUri = '/subscriptions/' + subscriptionId + '/resourceGroups/' + resourceGroupName + '/providers/microsoft.insights/components/' + appInsightsResourceName;
     apiKeyProperties.linkedReadProperties = [ basePropertiesUri + '/api',
-    basePropertiesUri + '/draft',
-    basePropertiesUri + '/extendqueries',
-    basePropertiesUri + '/search',
-    basePropertiesUri + '/aggregate'];
+        basePropertiesUri + '/draft',
+        basePropertiesUri + '/extendqueries',
+        basePropertiesUri + '/search',
+        basePropertiesUri + '/aggregate'];
 
-    client.aPIKeys.create(resourceGroupName, appInsightsResourceName, apiKeyProperties).then((resp) => {
-        logger.debug('List Response:');
-        logger.debug(resp);
-        logger.info('API Key Name: ' + apiKeyProperties.name);
-        logger.info('API Key ID: ' + resp.id.split('/apikeys/')[1]);
-        logger.info('API Key: ' + resp.apiKey);
-    }).catch((err) => {
-    logger.info('An error ocurred');
-    logger.info(err);
+    return new Promise(
+    function (resolve, reject) {
+        client.aPIKeys.create(rgName, resourceName, apiKeyProperties,
+        (err, data) => {
+            if (err) {
+                logger.error('An error ocurred', err);
+                reject(err);
+            }
+            else {
+                resolve(data);
+            }
+        });
     });
 }
 
-if ( options.keyOperation == "delete" ) {
-    /**
-     * Delete App Insights API Key
-     */
-    if ( options.keyId === null || options.keyId === undefined ) {
+/**
+* Delete App Insights API Key
+*
+* @returns {Promise}
+*/
+function deleteAppInsightApiKey(rgName, resourceName, keyId) {
+    if ( keyId === null || keyId === undefined ) {
         throw new Error('keyId cannot be null or undefined when delete has been specified');
     }
-    client.aPIKeys.deleteMethod(resourceGroupName, appInsightsResourceName, options.keyId).then((resp) => {
-        logger.info('Delete Response:');
-        logger.info(resp);
-    }).catch((err) => {
-    logger.info('An error ocurred');
-    logger.info(err);
+
+    return new Promise(
+    function (resolve, reject) {
+        client.aPIKeys.deleteMethod(rgName, resourceName, keyId,
+        (err, data) => {
+            if (err) {
+                logger.error('An error ocurred', err);
+                reject(err);
+            }
+            else {
+                resolve(data);
+            }
+        });
     });
 }
 
-if ( options.logLevel == "debug" || options.logLevel == "silly" ) {
-    /**
-     * List App Insight API Keys
-     */
-    client.aPIKeys.list(resourceGroupName, appInsightsResourceName).then((keys) => {
-        logger.debug('List of keys:');
-        logger.debug(keys);
-    }).catch((err) => {
-        logger.info('An error ocurred');
-        logger.info(err);
-    });
-
-    /**
-     * List App Insight Instances
-     */
-    client.components.list().then((components) => {
-        logger.debug('List of components:');
-        logger.debug(components);
-    }).catch((err) => {
-    logger.info('An error ocurred');
-    logger.info(err);
+/**
+* List all App Insight Instances
+*
+* @returns {Promise}
+*/
+function listAppInsightInstances() {
+    return new Promise(
+    function (resolve, reject) {
+        client.components.list(
+        (err, data) => {
+            if (err) {
+                logger.error('An error ocurred', err);
+                reject(err);
+            }
+            else {
+                resolve(data);
+            }
+        });
     });
 }
 
+/**
+* List App Insight API Keys
+*
+* @returns {Promise}
+*/
+function listAppInsightApiKeys(rgName, resourceName) {
+    return new Promise(
+    function (resolve, reject) {
+        client.aPIKeys.list(rgName, resourceName,
+        (err, data) => {
+            if (err) {
+                logger.error('An error ocurred', err);
+                reject(err);
+            }
+            else {
+                resolve(data);
+            }
+        });
+    });
+}
