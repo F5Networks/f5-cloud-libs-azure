ecb5906621207841bc320ac31a8f38357cd6c0d5
James Sevedge <j.sevedge@f5.com>
2018-03-30 19:41:21 -0700

    Resolve backup ucs sync --static

--- scripts/autoscale.sh
+++ scripts/autoscale.sh
@@ -194,7 +194,7 @@ icall_script_name="ClusterUpdate"
 # First check if iCall already exists
 tmsh list sys icall handler | grep $icall_handler_name
 if [[ $? != 0 ]]; then
-    tmsh create sys icall script $icall_script_name definition { exec $script_loc }
+    tmsh create sys icall script $icall_script_name definition { exec bash $script_loc }
     tmsh create sys icall handler periodic /Common/$icall_handler_name { first-occurrence now interval 120 script /Common/$icall_script_name }
 else
     echo "Appears the $icall_handler_name icall already exists!"
@@ -245,11 +245,12 @@ fi
 if [[ ! -z $backup_ucs ]]; then
     icall_handler_name="BackupUCSHandler"
     icall_script_name="BackupUCS"
+    script_loc="/config/cloud/backupUcsScript.sh"
     # First check if iCall already exists
     tmsh list sys icall handler | grep $icall_handler_name
     if [[ $? != 0 ]]; then
-        backup_ucs_cmd="/usr/bin/f5-rest-node /config/cloud/azure/node_modules/@f5devcentral/f5-cloud-libs/scripts/azure/runScripts.js --base-dir /config/cloud/azure/node_modules/@f5devcentral/f5-cloud-libs --log-level $log_level --autoscale \"--cloud azure --log-level $log_level --output /var/log/cloud/azure/autoScaleScript.log --host localhost --port $mgmt_port --user $user --password-url file://$passwd_file --password-encrypted $static $external_tag --provider-options scaleSet:$vmss_name,azCredentialsUrl:file://$azure_secret_file,resourceGroup:$resource_group --cluster-action backup-ucs --max-ucs-files $backup_ucs\""
-        tmsh create sys icall script $icall_script_name definition { exec $backup_ucs_cmd }
+        echo "/usr/bin/f5-rest-node /config/cloud/azure/node_modules/@f5devcentral/f5-cloud-libs/scripts/azure/runScripts.js --base-dir /config/cloud/azure/node_modules/@f5devcentral/f5-cloud-libs --log-level $log_level --autoscale \"--cloud azure --log-level $log_level --output /var/log/cloud/azure/autoScaleScript.log --host localhost --port $mgmt_port --user $user --password-url file://$passwd_file --password-encrypted $static $external_tag --provider-options scaleSet:$vmss_name,azCredentialsUrl:file://$azure_secret_file,resourceGroup:$resource_group --cluster-action backup-ucs --max-ucs-files $backup_ucs\"" > $script_loc
+        tmsh create sys icall script $icall_script_name definition { exec bash $script_loc }
         tmsh create sys icall handler periodic /Common/$icall_handler_name { first-occurrence `date +%Y-%m-%d`:23:59:59 interval 86400 script /Common/$icall_script_name }
     else
         echo "Appears the $icall_handler_name icall already exists!"
